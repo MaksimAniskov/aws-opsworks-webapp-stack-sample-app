71f30c82fde02171c42fce596df098df1f0435ba
Maksim Aniskov <MaksimAniskov@gmail.com>
2018-11-11 19:34:12 +0100

    Display EC2 instance id

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+const http = require('http');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  http.get('http://169.254.169.254/latest/meta-data/instance-id', resp => {
+    let data = '';
+
+    resp.on('data', chunk => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      res.send(`Hello World! My EC2 instance id is ${data}`);
+    })
+  })
+  .on('error', err => {
+    console.error(err.message);
+    res.send(`Hello World! But I don't know my EC2 instance id because of ${err.message}`);
+  });
+});
 
 app.listen(port, function() {
-    console.log('Listening on port %s', port);
+  console.log(`Listening on port ${port}`);
 });
